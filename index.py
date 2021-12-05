e3149e9053eefe0bccc9d0ea5409bc0c993a77e0
Seba <sebastian.gonzalez.cass@usach.cl>
2021-12-05 12:41:29 -0300

    Inicio del proyecto

---
+++ index.py
@@ -0,0 +1,33 @@
+def information_input():
+    """
+    Esta función le mostrará al usuario, y un menú para lo que el
+    usuario estime conveniente,
+    """
+    option = int(input("Bienvenid@ a Organizador digital, nos adaptamos"
+                    " rápidamente a ti :D!\n"
+                    "\n"
+                    "\n"
+                    "¿Qué desea hacer?\n"
+                    "  1. Comenzar o volver a responder las preguntas.\n"
+                    "  2. Ver mi horario.\n"
+                    "  3. Modificar mi horario.\n"
+                    "  4. ¿Cómo te sientes?.\n"
+                    "  5. ¿Cómo se usa esta aplicación?.\n"
+                    "  6. Salir.\n"
+                    "Seleccione una opción: "))
+    return option
+
+
+def questions():
+    answers = [0,0,0,0,0,0,0,0,0,0,0,0]
+    day = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+    answers_1 = input("Para comenzar, ingrese su horario de clases del día"
+                    " Lunes separado por ; (Ej: 9:00-10:00;10:00-11:00;etc.)"
+                    ": ")
+    return
+
+questions()
+
+def main():
+    return
+
