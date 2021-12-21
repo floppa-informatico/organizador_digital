d3d212bbb9911e5de8cbe6da1e3f2674a0d8c3e2
Seba <sebastian.gonzalez.cass@usach.cl>
2021-12-21 15:16:11 -0300

    reset

---
+++ base_tkinter.py
@@ -0,0 +1,27 @@
+import tkinter
+
+# Crear una ventana
+ventana = tkinter.Tk()
+
+# Cambiar tamaño de pantalla
+ventana.geometry("400x300")
+
+# Estiquetas
+
+# Crear etiqueta, en donde se va a poner esta etiqueta, en donde va a vivir
+# etiqueta = tkinter.Label(ventana, text = "hola mundo", bg = "pink")
+# Poner algo en pantalla, como argumento poner side es poner el lugar donde se pondrá la etiqueta
+# etiqueta.pack(side = tkinter.RIGHT)
+# Poder estirar el texto se usa como argumento fill para x
+# etiqueta.pack(fill = tkinter.X)
+# Estirar texto por el eje y
+# etiqueta.pack(fill = tkinter.Y, expand = True)
+# Estirar texto por toda la pantalla
+# etiqueta.pack(fill = tkinter.BOTH, expand = True)
+
+# Botones
+
+
+
+# Lleva el registro de todo lo que sucede en el programa
+ventana.mainloop()
