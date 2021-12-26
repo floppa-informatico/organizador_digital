e4ca22c70ccd18416ff60484425673c384796c6e
Seba <sebastian.gonzalez.cass@usach.cl>
2021-12-26 18:33:14 -0300

    Se agrega archivos al gitignore y se comienza a hacer el programa en tkinter

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Este es el archivo principal que se usará 
+"""
+from tkinter import *
+
+# Creamos la ventana
+menu_principal = Tk()
+
+
+# Muestra todo en pantalla y responde a la entrada del usuario hasta 
+# que el programa se termina
+menu_principal.mainloop()
